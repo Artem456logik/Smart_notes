90e6070d7bbcac151eabb91f7f88d8eb8752645f
art <art@gmail.com>
2024-12-08 13:31:57 +0200

    assda

--- main.py
+++ main.py
@@ -20,14 +20,8 @@ NOTES = {
     },
 }
 
-with open("notes_data.json", "w", encoding='UTF-8') as file:
-    json.dump(NOTES, file)
-
 with open("notes_data.json", "r", encoding='UTF-8') as file:
     NOTES = json.load(file)
-
-for note in NOTES:
-    ui.notes_list.addItem(note)
 
 ui.notes_list.addItems(NOTES)
 
@@ -61,9 +55,79 @@ def save_note():
             "теги": []
         }
 
+        NOTES[note_name]["текст"] = note_text
+
         with open("notes_data.json", "w", encoding='UTF-8') as file:
             json.dump(NOTES, file)
 ui.save_note.clicked.connect(save_note)
+
+def del_note():
+    if ui.notes_list.selectedItems():
+        note_name = ui.notes_list.currentItem().text()
+
+        del NOTES[note_name]
+    
+        ui.textEdit.clear()
+
+        ui.notes_list.clear()
+        ui.notes_list.addItems(NOTES)
+
+        with open("notes_data.json", "w", encoding="UTF-8") as file:
+            json.dump(NOTES, file)
+
+ui.btn_delette_note.clicked.connect(del_note)
+
+def add_tag():
+    if ui.notes_list.selectedItems():
+        if ui.tag_search.text():
+            note_name = ui.notes_list.currentItem().text()
+            new_tag = ui.tag_search.text()
+            if not new_tag in NOTES[note_name]["теги"]:
+                NOTES[note_name]["теги"].append(new_tag)
+
+                ui.tags_list.addItem(new_tag)
+                ui.tag_search.clear()
+
+                with open("notes_data.json", "w", encoding="UTF-8")as file:
+                    json.dump(NOTES, file)
+                    
+
+ui.btn_add_tag.clicked.connect(add_tag)
+
+def del_tag():
+    if ui.notes_list.selectedItems():
+        note_name = ui.notes_list.currentItem().text()
+        if ui.tags_list.selectedItems():
+            tag = ui.tags_list.currentItem().text()
+            NOTES[note_name]["теги"].remove(tag)
+
+            ui.tags_list.clear()
+            ui.tags_list.addItem(NOTES[note_name]["теги"])
+            with open("notes_data.json", "w", encoding="UTF-8")as file:
+                json.dump(NOTES, file)
+
+ui.btn_delete_tag.clicked.connect(del_tag)
+
+def search_tag():
+        tag = ui.tag_search.text()
+        filtered_notes = {}
+        for note_name, note in NOTES.items():
+            if tag in note["теги"]:
+                filtered_notes[note_name] = note
+
+        ui.notes_list.clear()
+        ui.notes_list.addItems(filtered_notes)
+
+        ui.btn_search_tag.setText("скинути пошук")
+
+    elif ui.btn_search_tag.text() == "Скинути пошук":
+        ui.notes_list.clear()
+        ui.notes_list.addItems(NOTES)
+        ui.btn_search_tag.setText("Шукати по тегу")
+        ui.tag_search.clear()
+
+
+ui.btn_search_tag.clicked.connect(search_tag)
 
 win.show()
 app.exec()
